145fc28c8146fa9b76164b0b2db6e61b33240f84
Teja <munipallaesaiteja29@email.com>
2026-04-20 17:07:10 +0530

    Configured framework to capture screenshots, videos, and traces on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,15 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+ use: {
+    /* Capture screenshot automatically on failure */
+    screenshot: 'only-on-failure',
+    
+    /* Record video automatically on failure */
+    video: 'retain-on-failure',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* Collect full trace on failure for debugging */
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
